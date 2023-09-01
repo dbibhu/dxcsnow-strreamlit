c9212cfcdb7e59caaf76b7a5b8f9a1943ecd4130
dbibhu <dbibhu@gmail.com>
2023-09-01 13:09:39 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -9,7 +9,15 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("select * from FRUIT_LOAD_LIST")
+my_data_row = my_cur.fethchone()
+streamlit.text("the fruit load contain list")
+streamlit.text(my_data_row)
+
+
 streamlit.header("Fruityvice Fruit Advice!")
+
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
